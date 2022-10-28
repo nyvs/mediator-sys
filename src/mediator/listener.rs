740e49a5603c78d33227bf22504f5a4d2c2387a7
nyvs <jwstrathmann@gmail.com>
2022-10-28 17:09:29 +0200

    explain Listener

--- src/mediator/listener.rs
+++ src/mediator/listener.rs
@@ -1,6 +1,7 @@
 use core::fmt::Debug;
 
-/// A [`Listener`] is a closure that is generic over its received event `Ev`.
+/// A [`Listener`] is a user-defined closure that is generic over its received event `Ev`.
+/// The closure handles the event and may act upon an event.
 pub trait Listener<Ev: Debug>: Fn(Ev) -> () + Send + 'static {}
 
 impl<Ev> Debug for dyn Listener<Ev>
